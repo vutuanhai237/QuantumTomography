86739742702830875fe016d0de93e785ade80bdf
DanLinhHuynh-Niwashi <huynhdanlinh080204@gmail.com>
2025-09-16 15:43:04 +0900

    Refactor: Create new ready to use tomography class for adam learning and kraus based tomography

--- qtomo/optimizer.py
+++ qtomo/optimizer.py
@@ -6,9 +6,70 @@
 # https://github.com/vutuanhai237/qoop/blob/master/core/optimizer_pennylane.py
 # Optimizer thì ko nhận bát kì tham số nào khác ngoài hyperparameter của chính nào và gradient/cost_fn, theta
 
+from qtomo import gradient
+from qtomo import utils
+import tensorflow as tf
 
 class Optimizer:
-    pass
+    def __init__(self, alpha=0.001):
+        """
+        Initialize the optimizer.
 
+        Args:
+            alpha (float): Learning rate.
+        """
+        self.alpha = alpha
+    def step_and_cost(self, objective_fn, theta, grad_fn):
+        pass
+    
+class SGDOptimizer(Optimizer):
+    def step_and_cost(self, objective_fn, theta, grad_fn):
+        pass
+    
 class AdamOptimizer(Optimizer):
-    pass
+    def __init__(self, alpha=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.alpha = alpha
+        self.beta1 = tf.constant(beta1, dtype=tf.complex128)
+        self.beta2 = tf.constant(beta2, dtype=tf.complex128)
+        self.epsilon = epsilon
+
+        self.m = None
+        self.v = None
+
+        self.t = tf.constant(0, dtype=tf.complex128)
+    
+    def step_and_cost(self, 
+                      param: tf.Variable, 
+                      loss_fn: callable, 
+                      grad_fn: callable = gradient.calculate_gradient
+                     ) -> tuple[tf.Variable, tf.Tensor]:
+        """
+        Perform one Adam optimization step with projection on the parameter.
+        
+        Args:
+          param (tf.Variable): Complex tensor parameter to optimize.
+          loss_fn (callable): Function that takes `param` and returns scalar loss tensor.
+          grad_fn (callable): Function that calculates the projected gradient, default is gradient.calculate_gradient.
+        
+        Returns:
+          param (tf.Variable): Updated parameter after one optimization step.
+          loss (tf.Tensor): Scalar loss value *before* the parameter update.
+        """
+        if self.m is None:
+            self.m = tf.zeros_like(param, dtype=tf.complex128)
+            self.v = tf.zeros_like(param, dtype=tf.complex128)
+        
+        self.t += 1
+        
+        # Compute loss before update (for gradient calculation)
+        proj_grad, loss = grad_fn(loss_fn, param)
+
+        self.m = self.beta1 * self.m + (1 - self.beta1) * proj_grad
+        self.v = self.beta2 * self.v + (1 - self.beta2) * tf.math.square(proj_grad)
+
+        m_hat = self.m / (1 - tf.pow(self.beta1, self.t))
+        v_hat = self.v / (1 - tf.pow(self.beta2, self.t))
+
+        update = param - self.alpha * m_hat / (tf.math.sqrt(v_hat) + self.epsilon)
+        
+        return update, loss
--- qtomo/tomo.py
+++ qtomo/tomo.py
@@ -3,13 +3,96 @@
 # Lưu trữ data, model, optimizer, metric, generator
 # Gom lại từ các fuke
 
-
-
-
-
-
+from qtomo import generator
+from qtomo import gradient
+from qtomo import metric
+from qtomo.optimizer import AdamOptimizer, SGDOptimizer
+from qtomo import utils
+from qtomo import quantum_ops
 class Tomography:
-    pass
+    def __init__(self):
+        self.target_rho_set = None  # Training resistivity for projection
+        self.gradient_fn = gradient.calculate_gradient  # Gradient function
+        self.loss_fn = metric.mean_infidelity  # Loss function
+        self.evaluate_fn = metric.compilation_trace_fidelity  # Loss function
+        self.data_process_fn = None # Target process as function
+
+    def init_training_set(self, num_qubits, num_rho):
+        self.target_rho_set = generator.haar_probe_states(num_qubits, num_rho)
+
+    def fit(self, *args, **kwargs):
+        pass
+
+class KrausTomography(Tomography):
+    def __init__(self):
+        super().__init__()
+        self.kraus_operators = None  # Current Kraus operators
+
+    def init(self, num_qubits, num_rho, num_kraus):
+        dim = 2 ** num_qubits
+        self.kraus_operators = generator.kraus_operators(dim=dim, num_operators=num_kraus)
+        self.init_training_set(num_qubits, num_rho)
+
+    def calculate_loss(self, kraus_operators):
+        rho_final_set = self.data_process_fn(self.target_rho_set, kraus_operators)
+        loss = self.loss_fn(rho_final_set, self.target_rho_set)
+        return loss
+    
+    def fit(self, epochs=100, optimizer_name='adam', optimizer_params=None):
+        if optimizer_params is None:
+            optimizer_params = {}
+
+        if optimizer_name.lower() == 'adam':
+            # Gán default nếu người dùng không truyền
+            defaults = {'alpha': 0.001, 'beta1': 0.9, 'beta2': 0.999, 'epsilon': 1e-8}
+            # Cập nhật defaults với params do người dùng truyền
+            defaults.update(optimizer_params)
+            self.optimizer = AdamOptimizer(**defaults)
+
+        elif optimizer_name.lower() == 'sgd':
+            defaults = {'alpha': 0.01}
+            defaults.update(optimizer_params)
+            self.optimizer = SGDOptimizer(**defaults)
+
+        else:
+            raise ValueError(f"Unsupported optimizer: {optimizer_name}")
+
+        param = self.kraus_operators
+
+        for epoch in range(epochs):
+            param, loss = self.optimizer.step_and_cost(param, self.calculate_loss, self.gradient_fn)
+            param = utils.normalize_kraus(param)
+            print(f"Epoch {epoch+1}/{epochs}, loss: {loss.numpy()}")
+
+        self.kraus_operators = param
+        return param
+    
+    def evaluate(self, rho_input, target_process_fn):
+        """
+        Apply the learned process to a new input state.
+
+        Args:
+            rho_input (DensityMatrix): Input density matrix.
+
+        Returns:
+            list: Processed output states from the learned model.
+        """
+        if self.data_process_fn is None:
+            raise ValueError("Could not find a data process method.")
+        rho_output = quantum_ops.apply_kraus_operators(rho_input, self.kraus_operators)
+        rho_target = target_process_fn(rho_input)
+        comparison_result = self.evaluate_fn(rho_output, rho_target)
+        return rho_output, rho_target, comparison_result
+    
+class ChoiTomography(Tomography):
+    def __init__(self):
+        super().__init__()
+        self.unitary = None  # Current unitary operator
+
+    def init(self, num_qubits, num_rho):
+        self.unitary = generator.choi_matrix(num_qubits)
+        self.init_training_set(num_qubits, num_rho)
+
 # Chủ yếu bao gồm input (unitary, epsilon, ...) và tập Kraus operator
 # Qúa trình tính toắn update theo kiểu sau
 # Tomography.init() => khởi tạo Kraus operator, gọi từ generator.py
